19cc245bc2e1e749d9f99363da2d7f09270bc6a0
Mohammadreza <mrzfvl3908@gmail.com>
2025-12-01 12:17:50 +0330

    edit admin
    edit admin and edit sql

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog_app.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','status','created_date']
+    date_hierarchy = 'created_date'
+    search_fields = ['title', 'short_description']
+    list_filter = ['status']
+    empty_value_display = '-empty-'
+
+admin.site.register(Post, PostAdmin)
