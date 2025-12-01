9c91cb32cd8e97fa9feff61ebd1634390fe27b84
Mohammadreza <mrzfvl3908@gmail.com>
2025-12-01 12:19:21 +0330

    Revert "edit admin in blog_app"
    This reverts commit 19cc245bc2e1e749d9f99363da2d7f09270bc6a0.

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from blog_app.models import Post
 
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','status','created_date']
-    date_hierarchy = 'created_date'
-    search_fields = ['title', 'short_description']
-    list_filter = ['status']
-    empty_value_display = '-empty-'
-
-admin.site.register(Post, PostAdmin)
+# Register your models here.
