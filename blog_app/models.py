36a65c6ac1e4d47fe7335b250ff7533fce48db66
Mohammadreza <mrzfvl3908@gmail.com>
2025-12-01 12:05:28 +0330

    add model blog_app
    add model blog_app and migrate

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 
+class Post(models.Model):
+    #category
+    #author
+    #image
+    title = models.CharField(max_length=200)
+    short_description = models.TextField()
+    description = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
