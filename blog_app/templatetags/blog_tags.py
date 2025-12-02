c61e4e834f99d925ca9d6d535ada63d38f663528
Mohammadreza <mrzfvl3908@gmail.com>
2025-12-03 01:59:43 +0330

    add inclusion category

--- blog_app/templatetags/blog_tags.py
+++ blog_app/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
-from blog_app.models import Post
+from django.db.models import Count, Q
+
+from blog_app.models import Post, Category
 
 register = template.Library()
 
@@ -7,3 +9,11 @@ register = template.Library()
 def recent_posts():
     rec_posts = Post.objects.order_by('-created_date')[:3]
     return {'rec_posts': rec_posts}
+
+
+@register.inclusion_tag('blog_app/includes/category_posts.html')
+def category_posts():
+    categories = Category.objects.annotate(
+        post_count=Count('post', filter=Q(post__status=1))
+    )
+    return {'categories': categories}
